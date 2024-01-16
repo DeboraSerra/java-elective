41a7aa99919c0950418e4674c3e5cfb5ecc8e651
Débora <debora.r.serra@gmail.com>
2024-01-16 14:09:57 -0300

    feat: add first lessons

---
+++ executing-and-debugging/src/SomaNumeros.java
@@ -0,0 +1,12 @@
+public class SomaNumeros {
+  /**
+   * retorna a soma de dois números
+   * @param numA um número inteiro
+   * @param numB um número inteiro
+   * @return a soma entre os dois números
+   */
+  public static int retornaSomaEntreDoisNumeros(int numA, int numB) {
+    int soma = numA + numB;
+    return soma;
+  }
+}
