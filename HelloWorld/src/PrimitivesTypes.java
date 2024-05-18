c9e832875b46ca066dd50f5e5f52b65a144bc420
Débora Serra <debora.r.serra@gmail.com>
2024-05-18 16:06:00 -0300

    feat: add primitive types class

---
+++ HelloWorld/src/PrimitivesTypes.java
@@ -0,0 +1,15 @@
+public class PrimitivesTypes {
+    public static void main(String[] args) {
+        float numA = 10.5f;
+        double numB = 10.51;
+        double numC = 10.51d;
+        System.out.println(numA - numB);
+        System.out.println(numA + numC);
+        System.out.println(numB + numC);
+
+        char letterC = 'c';
+        char alsoLetterC = 99;
+        System.out.println(letterC);
+        System.out.println(alsoLetterC);
+    }
+}
